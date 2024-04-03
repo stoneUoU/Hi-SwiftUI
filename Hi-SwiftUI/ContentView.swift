4ef19ce480823549cef52891ffc22dfc6068e45e
stone <stoneUoU@163.com>
2024-04-03 17:03:28 +0800

    Initial Commit

---
+++ Hi-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hi-SwiftUI
+//
+//  Created by stone on 2024/4/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hi-SwiftUI/Hi_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hi_SwiftUIApp.swift
+//  Hi-SwiftUI
+//
+//  Created by stone on 2024/4/3.
+//
+
+import SwiftUI
+
+@main
+struct Hi_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
